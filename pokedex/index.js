83b8751f9b25b0880e9e3d61fb74644d2b02860f
Anna Kuznetsova <Aniuta907email@gmail.com>
2020-05-13 10:26:40 +0300

    Added minimist

--- pokedex/index.js
+++ pokedex/index.js
@@ -1,11 +1,37 @@
-const http = require("http");
+const meow = require('meow');
+const minimist = require('minimist');
  
-let message = "Hello World!";
-http.createServer(function(request,response){
-     
-    console.log(message);
-    response.end(message);
-     
-}).listen(3000, "127.0.0.1",()=>{
-    console.log("Сервер начал прослушивание запросов");
+const cli = meow(`
+    Options
+    --name, -n Set a file name in the output directory
+    --file, -f Select file from the input directory
+    --length, -l Limit the length of the entered text
+    --help, -h Output a hint
+ 
+    Examples
+    $ node index.js --file index --name foo
+    $ node index.js --length 10
+    $ node index.js --help
+`, {
+    flags: {
+        help: {
+            type: 'boolean',
+            alias: 'h'
+        }
+    }
 });
+
+const args = minimist(process.argv.slice(2), {
+    alias: {
+        h: 'help',
+        n: 'name',
+        f: 'file',
+        l: 'length',
+    },
+    unknown: (arg) => {
+        console.error('Invalid parameter:', arg);
+        meow.showHelp();
+        return false;
+    }
+});
+console.log(args);
