97442ca595d71386a843a0ff1be8bac4d0d522ca
Anna Kuznetsova <Aniuta907email@gmail.com>
2020-06-01 09:38:04 +0300

    Add get request

--- pokedex/crud.js
+++ pokedex/crud.js
@@ -7,12 +7,33 @@ server.use(express.json());
 const db = JSON.parse(fs.readFileSync('./db.json'));
 
 //GET all pokemons
-server.get('/pokemons', function(req, res) {});
+server.get('/pokemons', function(req, res) {
+	res.status(200).json({
+		status: 'Welcome to the world of pokemons',
+		data: {
+			pokemons: db
+		}
+	});
+});
 
 //GET pokemon with specified id
 server.get('/pokemons/:pokemonId', function(req, res) {
-	const id = parseInt(req.params.id);
-	const pok = db.find((pokemon) => pokemon.id === id);
+	const pokemonId = parseInt(req.params.id);
+	const pok = db.find((pokemon) => pokemon.id === pokemonId);
+
+	if (pok === undefined || pok === null) {
+		return res.status(404).json({
+			status: 'fail',
+			message: 'Invalid Id'
+		});
+	}
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			pok
+		}
+	});
 });
 
 //POST new pokemon
@@ -26,13 +47,18 @@ server.post('/pokemons', function(req, res) {
 		...req.body
 	};
 	db.push(newPokemon);
+	res.send('Add a pokemon');
 });
 
 //PUT edited pokemon in-place of pokemon with specified id
-server.put('/pokemons/:pokemonId', function(req, res) {});
+server.put('/pokemons/:pokemonId', function(req, res) {
+	res.send('Update the pokemon');
+});
 
 //DELETE pokemon with specified id
-server.delete('/pokemons/:pokemonId', function(req, res) {});
+server.delete('/pokemons/:pokemonId', function(req, res) {
+	res.send('Delete the pokemon');
+});
 
 //START SERVER
 server.listen(3000, function() {
